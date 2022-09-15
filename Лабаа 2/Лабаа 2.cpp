0c10f53834add92b65dd068bcaeadd005907db31
taran.daniil03@mail.ru <taran.daniil03@mail.ru>
2022-09-15 16:31:58 +0700

    Добавлен класс Deck с вложенным классов Card. Добавлены два конструктора и один деструктор для Deck. Компиляция успешна

--- Лабаа 2/Лабаа 2.cpp
+++ Лабаа 2/Лабаа 2.cpp
@@ -1,6 +1,32 @@
 ﻿
 #include <iostream>
+typedef struct Deck { // Колода карт.
+    typedef struct Card {// Карта. Имеет стоимости от 1 до 10.
+        int cost;
+    };
+    Card cards[10]; // Массив карт. 
+    int capacity;
+};
+// Конструкторы
+Deck* Init_Deck() { // По умолчанию. Размер 10
+    Deck* ptr;
+    ptr = (Deck*)malloc(sizeof(Deck));
+    ptr->capacity = 10;
+    return ptr;
+}
 
+Deck* Init_Deck(int capacity) { // Размер по выбору
+    Deck* ptr;
+    ptr = (Deck*)malloc(sizeof(Deck));
+    ptr->capacity = capacity;
+    return ptr;
+}
+//
+
+// Деструктор
+void destr_Deck(Deck* ptr) {
+    delete(ptr);
+}
 int main()
 {
     std::cout << "Hello World!\n";
