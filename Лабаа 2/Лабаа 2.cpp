7126fe651d9a167bf8a426d1082ac96684729f6b
taran.daniil03@mail.ru <taran.daniil03@mail.ru>
2022-09-15 17:13:00 +0700

    Дополнение к методам Cards и Deck.

--- Лабаа 2/Лабаа 2.cpp
+++ Лабаа 2/Лабаа 2.cpp
@@ -4,7 +4,7 @@ typedef struct Card {// Карта. Имеет стоимости от 1 до 10
     int cost;
 };
 typedef struct Deck { // Колода карт.
-    Card cards[10]; // Массив карт. 
+    Card* cards[10]; // Массив карт. 
     int capacity;
 };
 // Конструкторы Deck
@@ -12,6 +12,10 @@ Deck* Init_Deck() { // По умолчанию. Размер 10
     Deck* ptr;
     ptr = (Deck*)malloc(sizeof(Deck));
     ptr->capacity = 10;
+    for (int i = 0; i < ptr->capacity; i++) {
+        ptr->cards[i] = NULL;
+            
+    }
     return ptr;
 }
 
@@ -29,24 +33,35 @@ void destr_Deck(Deck* ptr) {
 }
 
 // Конструктор Card
-Card* Init_Card(int cost) {
+Card* Init_Card(int cost) {// NULL указатель при ошибке
     Card* ptr;
     if (cost > 0 && cost < 11) {
         ptr = (Card*)malloc(sizeof(Card));
         ptr->cost = cost;
         return ptr;
     }
-    else {
-        printf("\nОшибка. Неверное значение стоимости карты. Указатель на объект равен нулю \n");
-        return 0;
-    }
+    else 
+        return NULL;
+
 }
 // Деструктор Card
 void destr_Card(Card* ptr) {
     delete(ptr);
 }
 
-// Методы 
+// Методы Deck
+int Get_Num_Cards(Deck* deck_ptr) {// Получение кол-ва карт в колоде
+    int num = 0;
+    for (int i = 0; i < deck_ptr->capacity; i++) {
+        if (deck_ptr->cards[i] != NULL)
+            num++;
+    }
+    return num;
+}
+void Set_Card_In_Place(Deck* deck_ptr,Card* card_ptr, int place) {// Установить карту в определённое место
+    deck_ptr->cards[place] = card_ptr;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
