827b2c334a47f28c51891772b7bc45c84f10472a
taran.daniil03@mail.ru <taran.daniil03@mail.ru>
2022-09-19 19:49:10 +0700

    Начал проработку ф-ии main. Вроде все классы готовы. Всех методов ещё нет

--- Лабаа 2/Лабаа 2.cpp
+++ Лабаа 2/Лабаа 2.cpp
@@ -1,5 +1,5 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+using namespace std;
 typedef struct Card {// Карта. Имеет стоимости от 1 до 10.
     int cost;
 };
@@ -7,20 +7,37 @@ typedef struct Deck { // Колода карт.
     Card* cards[10]; // Массив карт. 
     int capacity;
 };
-typedef struct Multiplyer {// Совокупность коэффициентов
-    int hard_multipl;
-    int soft_multipl;
-};
+typedef struct Coefficient {// Совокупность коэффициентов
+    int hard_coefficient;
+    int soft_coefficient;
+} current_multiplayer;
 typedef struct Hand {// Рука игрока. Макс размер -- 3.
-    Card* cards_value[3];
+    Card* cards[3];
 
 };
 typedef struct Player { // Игрок
-    char name[20];
+    string name;
     Hand hand;
     int current_score;
 };
-// 
+
+// Конструктор Card
+Card* Init_Card(int cost) {// NULL указатель при ошибке
+    Card* ptr;
+    if (cost > 0 && cost < 11) {
+        ptr = (Card*)malloc(sizeof(Card));
+        ptr->cost = cost;
+        return ptr;
+    }
+    else
+        return NULL;
+
+}
+// Деструктор Card
+void destr_Card(Card* ptr) {
+    delete(ptr);
+}
+
 // Конструкторы Deck
 Deck* Init_Deck() { // По умолчанию. Размер 10
     Deck* ptr;
@@ -32,37 +49,16 @@ Deck* Init_Deck() { // По умолчанию. Размер 10
     }
     return ptr;
 }
-
 Deck* Init_Deck(int capacity) { // Размер по выбору
     Deck* ptr;
     ptr = (Deck*)malloc(sizeof(Deck));
     ptr->capacity = capacity;
     return ptr;
 }
-//
-
 // Деструктор Deck
 void destr_Deck(Deck* ptr) {
     delete(ptr);
 }
-
-// Конструктор Card
-Card* Init_Card(int cost) {// NULL указатель при ошибке
-    Card* ptr;
-    if (cost > 0 && cost < 11) {
-        ptr = (Card*)malloc(sizeof(Card));
-        ptr->cost = cost;
-        return ptr;
-    }
-    else 
-        return NULL;
-
-}
-// Деструктор Card
-void destr_Card(Card* ptr) {
-    delete(ptr);
-}
-
 // Методы Deck
 int Get_Num_Cards(Deck* deck_ptr) {// Получение кол-ва карт в колоде
     int num = 0;
@@ -75,8 +71,34 @@ int Get_Num_Cards(Deck* deck_ptr) {// Получение кол-ва карт в
 void Set_Card_In_Place(Deck* deck_ptr,Card* card_ptr, int place) {// Установить карту в определённое место
     deck_ptr->cards[place] = card_ptr;
 }
+// Конструктор Coefficient
+Coefficient* Init_Coefficient(int soft_coefficient, int hard_coefficient) {
+    Coefficient* ptr;
+    ptr = (Coefficient*)malloc(sizeof(Coefficient));
+    ptr->soft_coefficient = soft_coefficient;
+    ptr->hard_coefficient = hard_coefficient;
+    return ptr;
+}
+// Деструктор Coefficient
+void destr_Coefficient(Coefficient* ptr) {
+    delete(ptr);
+}
 
+// Конструктор Player (и Hand)
+Player* Init_Player(string name) {
+    Player* ptr_1;
+    Hand* ptr_2;
+    ptr_1 = (Player*)malloc(sizeof(Player));
+    ptr_1->name = name;
+    ptr_2 = (Hand*)malloc(sizeof(Hand));
+    return ptr_1;
+}
+// Деструктор Player
+void destr_Player(Player* ptr) {
+    delete(ptr);
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Создадим колоду из 5 карт на столе
+
 }
