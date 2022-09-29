c87e6451c2dc975789179e1384f648eaf8a762fa
taran.daniil03@mail.ru <taran.daniil03@mail.ru>
2022-09-29 21:39:35 +0700

    Привёл в порядок main, добавив вывод в консоль

--- Лабаа 2/Deck.cpp
+++ Лабаа 2/Deck.cpp
@@ -2,6 +2,7 @@
 
 Deck::Deck()
 {
+    this->capacity = 5;
     for (int i = 0; i < 10; i++)
     {
         this->card_flag[i] = 0;
@@ -10,6 +11,7 @@ Deck::Deck()
 
 Deck::Deck(int capacity)
 {
+    this->capacity = capacity;
     for (int i = 0; i < capacity; i++)
     {
         this->card_flag[i] = 0;
@@ -29,6 +31,7 @@ int Deck::Get_Num_Cards()
 void Deck::Set_Card_In_Place(Card* card_ptr, int place)
 {
     this->cards[place - 1] = card_ptr;
+    this->card_flag[place - 1] = 1;
 }
 
 Card* Deck::Get_Card_From_Place(int place)
--- Лабаа 2/Лабаа 2.cpp
+++ Лабаа 2/Лабаа 2.cpp
@@ -6,45 +6,102 @@
 #include "Hand.h"
 #include "Player.h"
 using namespace std;
+/*setlocale(LC_ALL, "rus");
+    // Создадим колоду вместимостью 5 карт
+    printf("Создадим колоду вместимостью 5 карт\n");
+    Deck* my_deck;
+    my_deck = Init_Deck(5);    
+    // Посмотрим кол-во карт в колоде (не макс. их кол-во)
+    printf("Посмотрим кол-во карт в колоде, созданную вначале \n");
+    int num;
+    num = Get_Num_Cards(my_deck);
+    printf("Кол-во кард в колоде: %d\n", num);
+    // Добавим карту 5 в позицию 1, и карту 2 в позицию 4.
+    printf("Добавим карту 5 в позицию 1, и карту 2 в позицию 4\n");
+    Card* first_card, *second_card;
+    first_card = Init_Card(5);
+    second_card = Init_Card(2);
+    Set_Card_In_Place(my_deck,first_card, 1);
+    Set_Card_In_Place(my_deck,second_card, 4);
+    printf("Карты добавили, теперь глянем сколько их всего \n");
+    num = Get_Num_Cards(my_deck);
+    printf("Кол-во кард в колоде: %d\n", num);
+    printf("\n");
+    // Теперь создадим игрока №1 с его игровой рукой. Пока что она пустая
+    printf("Теперь создадим игрока №1 с его игровой рукой. Пока что она пустая\n");
+    Player* my_player;
+    my_player = Init_Player(1);
+    // Получим карту в руку из колоды (из позиции колоды №4) в руку нашего игрока (в позицию руки №1)
+    printf("Получим карту в руку из колоды (из позиции колоды №4) в руку нашего игрока (в позицию руки №1)\n");
+    Set_Card_In_Place_Of_Hand(my_player,Get_Card_From_Place(my_deck,4), 1);
+    // Так же получим последнюю карту из колоды в нашу руку. Поставим карту в третью позицию руки
+    printf("Так же получим последнюю карту из колоды в нашу руку. Поставим карту в третью позицию руки\n");
+    Set_Card_In_Place_Of_Hand(my_player,Get_Card_From_Place(my_deck,1), 3);
+    printf("\n");
+    // Теперь создадим коэффициент для подсчёта очков
+    printf("Теперь создадим коэффициент для подсчёта очков\n");
+    Coefficient* my_coefficient;
+    my_coefficient = Init_Coefficient(25, 2);
+    printf("Hard coefficient первого= %d, Soft coefficient первого= %d\n", Get_Hard_Coefficient(my_coefficient),Get_Soft_Coefficient(my_coefficient));
+    printf("Посчитае кол-во очков на руке игрока. Их: %d\n", Get_Sum_Points(my_player,my_coefficient));
+    printf("\n");
+    system("pause");*/
 int main()
 {
     setlocale(LC_ALL, "rus");
     // Создадим колоду вместимостью 5 карт
+    printf("Создадим колоду вместимостью 5 карт\n");
     Deck my_deck(5);
     // Можем так же создать колоду через консольный диалог
+    printf("Можем так же создать колоду через консольный диалог\n");
     int buf;
     Deck* you_deck;
     printf("Введите кол-ва карт в колоде: ");
     scanf("%d", &buf);
     you_deck = new(Deck);
     //Сразу удалим её, т.к. она нам не нужна
+    printf("Сразу удалим её, т.к. она нам не нужна\n");
     delete(you_deck);
-    printf("\nВыша колода была создана и после удалена\n");
+    printf("\nВаша колода была создана и после удалена\n");
+    printf("\n");
     // Посмотрим кол-во карт в колоде (не макс. их кол-во)
+    printf("Посмотрим кол-во карт в колоде (не макс. их кол-во)\n");
     int num;
     num=my_deck.Get_Num_Cards();
     printf("Кол-во кард в колоде: %d\n", num);
     // Добавим карту 5 в позицию 1, и карту 2 в позицию 4. 
+    printf("Добавим карту 5 в позицию 1, и карту 2 в позицию 4\n");
     Card first_card(5), second_card(2);
     my_deck.Set_Card_In_Place(&first_card,1);
     my_deck.Set_Card_In_Place(&second_card, 4);
-    printf("Добавим карту 5 в позицию 1, и карту 2 в позицию 4. Операция выполнена\n");
+    num = my_deck.Get_Num_Cards();
+    printf("Кол-во кард в колоде теперь: %d\n", num);
+    printf("\n");
     // Теперь создадим игрока №1 с его игровой рукой. Пока что она пустая
+    printf("Теперь создадим игрока №1 с его игровой рукой. Пока что она пустая\n");
     Player my_player(1);
     // Получим карту в руку из колоды (из позиции колоды №4) в руку нашего игрока (в позицию руки №1)
+    printf("Получим карту в руку из колоды (из позиции колоды №4) в руку нашего игрока (в позицию руки №1)\n");
     my_player.hand.Set_Card_In_Place_Of_Hand(my_deck.Get_Card_From_Place(4), 1);
     // Так же получим последнюю карту из колоды в нашу руку. Поставим карту в третью позицию руки
+    printf("Так же получим последнюю карту из колоды в нашу руку. Поставим карту в третью позицию руки\n");
     my_player.hand.Set_Card_In_Place_Of_Hand(my_deck.Get_Card_From_Place(1), 3);
+    printf("\n");
     // Теперь создадим коэффициент для подсчёта очков
+    printf("Теперь создадим коэффициент для подсчёта очков\n");
     Coefficient my_coefficient(25,2);
     // Можем попробовать создать его разными конструкторами.
     Coefficient coefficient_1, coefficient_2(2);
-    printf("Hard coefficient первого= %d, Soft coefficient первого= %d\n", coefficient_1.Get_Hard_Coefficient(), coefficient_1.Get_Soft_Coefficient());
-    printf("Hard coefficient второго= %d, Soft coefficient второго= %d\n", coefficient_2.Get_Hard_Coefficient(), coefficient_2.Get_Soft_Coefficient());
+    printf("Hard coefficient = %d, Soft coefficient = %d\n", my_coefficient.Get_Hard_Coefficient(), my_coefficient.Get_Soft_Coefficient());
+    printf("\n");
+    printf("\n");
     //Так же покажем работу с динамическим массивом объектов класса и с массивом динамических объектов класса
-    printf("Поработаем с массивами\n");
-    // Массивом динамических объектов класса
+    // Массив динамических объектов класса
     // Создадим три карты со стоимостью 3 и узнаем их суммарную стоимость. После этого удалим
+    printf("Так же покажем работу с динамическим массивом объектов класса и с массивом динамических объектов класса\n");
+    printf("\n");
+    printf("Массив динамических объектов класса\n");
+    printf("Создадим три карты\n");
     Card* mas_cards[50];
     int sum=0;
     for (int i = 0; i < 3; i++)
@@ -60,6 +117,7 @@ int main()
     {
         delete(mas_cards[i]);
     }
+    printf("Работа с динамическим массивом объектов класса указана в комментариях\n");
     // Работа с динамическим массивом объектов класса.
     Card* arr = new Card[10];// Конструкто по умолчанию делает все карты стоимостью 1
     for (int i = 0; i < 10; ++i) {// Теперь стоимость установим в 3.
@@ -67,8 +125,11 @@ int main()
     }
     // Теперь удалим массив
     delete[] arr;
+    printf("\n");
     // Воспользуемся созданным коэффициентом для того, чтобы подсчитать кол-во баллов на руке нашего игрока
+    printf("Воспользуемся ранее созданным коэффициентом для того, чтобы подсчитать кол-во баллов на руке нашего игрока\n");
     int points;
     points = my_player.Get_Sum_Points(&my_coefficient);
+    printf("Кол-во баллов на руке нашего игрока: %d\n", points);
     system("pause");
 }
